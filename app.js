58d269f8d45caa1c158787c4451f7acbe1b6360b
liam_Office <Liam.xiao@outlook.com>
2016-10-13 10:48:38 +0800

    app update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var ejs = require();
+var ejs = require('ejs');
 
 var app = express();
 
